656b20f34faee463c53418bb7c5963cc31fd2bbe
kimcodex <kimgamana@gmail.com>
2018-05-31 21:12:11 +0800

    update comments template

--- comments.php
+++ comments.php
@@ -1,7 +1,7 @@
 <div class="comments">
 	<?php $args = array(
 		'walker'            => null,
-		'max_depth'         => '',
+		'max_depth'         => 2,
 		'style'             => 'ul',
 		'callback'          => null,
 		'end-callback'      => null,
@@ -14,18 +14,43 @@
 		'reverse_children'  => '',
 		'format'            => 'html5', // or 'xhtml' if no 'HTML5' theme support
 		'short_ping'        => false,   // @since 3.6
-	        'echo'              => true     // boolean, default is true
+	    'echo'              => true     // boolean, default is true
 	); ?>
 	<?php wp_list_comments( $args,$comments); 
-		$comments_args = array(
-		        'label_submit'=>'Send',
-		        'title_reply'=>'Write a Comment',
-		        'comment_notes_after' => '',
-		        'comment_field' => '<p class="comment-form-comment"><label for="comment">' . _x( 'Comment', 'noun' ) . '</label><br /><textarea id="comment" name="comment" aria-required="true"></textarea></p>',
+
+		$aria_req = ( $req ? " aria-required='true'" : '' );
+
+		$fields =  array(
+
+		  'author' =>
+		    '<p class="comment-form-author"><input id="author" class="form-control" name="author" placeholder="Name" type="text" autocomplete="off" value="' . esc_attr( $commenter['comment_author'] ) .
+		    '" size="30"' . $aria_req . ' /></p>',
+		  'email' =>
+		       '<p class="comment-form-email"><input id="email" class="form-control" name="email" placeholder="Email(Optional)" type="text" autocomplete="off" value="' . esc_attr(  $commenter['comment_author_email'] ) .
+		       '" size="30"' . $aria_req . ' /></p>'
 		);
 
-		comment_form($comments_args);
+		$comments_args = array(
+	        
+	        'title_reply_before' => '<h5 class="card-header">',
+	        'title_reply'=>'Leave a Comment:',
+	        'title_reply_to'    => __( 'Reply to %s' ),
+	        'cancel_reply_link' => __( 'Cancel' ),
+	        'title_reply_after' => '</h5>',
+	       	'comment_notes_before' => '',
+	        'comment_field' => '<p class="comment-form-comment"><textarea id="comment" class="form-control" name="comment" rows="5" placeholder="Comment here..." aria-required="true"></textarea></p>',
+	        // 'comment_notes_after' => '</div>',
+	        'fields' => apply_filters( 'comment_form_default_fields', $fields ),
+	        'label_submit'=>'Submit',
+	        'class_submit' => 'btn btn-primary',
+		);		
+
 	?>
+
+	<div class="card my-4">	
+		<?php  comment_form($comments_args); ?>
+	</div>
 
 
 </div>
+
